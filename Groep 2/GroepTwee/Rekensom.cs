744902ba818053e006ebe5b0615653a76be0585b
dropthesloth <35850954+dropthesloth@users.noreply.github.com>
2021-02-10 10:36:56 +0100

    Rekensom

---
+++ Groep 2/GroepTwee/Rekensom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        int correctAnswer = 5;
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("3 + 2 = " + correctAnswer.ToString());
+        }
+    }
+}
