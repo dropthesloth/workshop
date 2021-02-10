74831888a309f2b6cfff5fd792319c071bce7019
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-02-10 08:48:34 +0100

    setup

--- Groep 1/GroepEen/Program.cs
+++ Groep 1/GroepEen/Program.cs
@@ -6,7 +6,7 @@ namespace GroepEen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
